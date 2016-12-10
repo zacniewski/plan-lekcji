522fce1e4c40da14a3ee613ae3e0d6a7919c652c
Grzegorz Lieske <grzesiu.lieske@gmail.com>
2016-12-10 18:13:42 +0100

    dates works

--- js/script.js
+++ js/script.js
@@ -1,85 +1,92 @@
 class timeTable {
-    constructor(htmlId, startMonth, startDay, numberOfMonths, startYear) {
+    constructor(htmlId, startDate, numberOfMonths) {
         this.htmlId = htmlId;
-        this.startDay = startDay;
-        this.startMonth = startMonth;
         this.numberOfMonths = numberOfMonths;
-        this.startYear = startYear;
+        this.startDate = startDate;
     }
+
 
     drawTable() {
         $(this.htmlId).attr('style', 'table');
         $(this.htmlId).append(this.__getTableHeadersHtml().join(''));
-        $(this.htmlId).append(this.__getMondays().join(''));
+        $(this.htmlId).append(this.__getDayHeaders(1).join(''));
+    }
 
+    __cloneDate(date) {
+        return new Date(date.getTime());
     }
 
     __getTableHeadersHtml() {
         let html = [];
         html.push('<tr class="dark-gray">');
         html.push('<td colspan="2">Mc-e</td>');
-        let endMonth = this.startMonth + this.numberOfMonths;
-        for (let i = this.startMonth; i <= endMonth; i++) {
-            let monthOfWork = i;
-            let yearOfWork = this.startYear;
-            if (monthOfWork > 12) {
-                monthOfWork = i - 12;
-                yearOfWork++;
-            }
-            let numOfWeeks = this.__weeksinMonth(monthOfWork, yearOfWork);
-            html.push('<td colspan="' + numOfWeeks + '">' + this.__getRomanNum(monthOfWork) + '</td>')
+        let objectOfWork = this.__cloneDate(this.startDate);
+        console.log(objectOfWork);
+        for (let i = 1; i <= this.numberOfMonths; i++) {
+            let numOfWeeks = this.__weeksinMonth(objectOfWork);
+            html.push('<td colspan="' + numOfWeeks + '">' + this.__getRomanNum(objectOfWork.getMonth() + 1) + '</td>');
+            objectOfWork = this.__addMonth(objectOfWork);
         }
         html.push('<td rowspan="2">Sale</td>');
         html.push('</tr>');
         return html;
     }
 
-    __getMondays() {
+    __getDayHeaders(day) {
         let html = [];
         html.push('<tr class="dark-gray">');
         html.push('<td colspan="2">dni</td>');
-        let endMonth = this.startMonth + this.numberOfMonths;
-        for (let i = this.startMonth; i <= endMonth; i++) {
-            let nextDay = 1;
-            let monthOfWork = i;
-            let yearOfWork = this.startYear;
-            if (monthOfWork > 12) {
-                monthOfWork = i - 12;
-                yearOfWork++;
-            }
-            let numOfWeeks = this.__weeksinMonth(monthOfWork, yearOfWork);
-            let first = new Date(yearOfWork, monthOfWork - 1);
-            let mondays = this.__getSameDays(first, 1);
+        let objectOfWork = this.__cloneDate(this.startDate);
+        for (let i = 0; i < this.numberOfMonths; i++) {
+            objectOfWork.setDate(1);
+            let mondays = this.__getSameDays(objectOfWork, day);
             let lastMonthDayIncluded = false;
+            console.log(mondays, objectOfWork);
             for (let y = 0; y < mondays.length; y++) {
-                if (mondays[0] <= 7 && mondays[0] != 1 && i == this.startMonth && !lastMonthDayIncluded) {
-                    let dt = new Date(2016, monthOfWork - 1, mondays[0]);
+                if (mondays[0] <= 7 && mondays[0] != 1 && objectOfWork.getMonth() == this.startDate.getMonth() && !lastMonthDayIncluded) {
+                    let dt = this.__cloneDate(objectOfWork);
+                    dt.setDate(mondays[0]);
                     dt.setDate(dt.getDate() - 7);
                     html.push('<td>' + dt.getDate() + '</td>');
                     lastMonthDayIncluded = true;
                 }
                 html.push('<td>' + mondays[y] + '</td>');
-                nextDay += 7;
             }
+            objectOfWork = this.__addMonth(objectOfWork);
         }
         html.push('</tr>');
         return html;
     }
 
-    __weeksinMonth(m, y) {
+    __addMonth(date) {
+        let day = date.getDate();
+        date.setDate(15);
+        date = new Date(new Date(date).setMonth(date.getMonth() + 1));
+        date.setDate(day);
+        return date;
+    }
+
+    __weeksinMonth(date) {
+        let y = date.getFullYear();
+        let m = date.getMonth() + 1;
         y = y || new Date().getFullYear();
         let d = new Date(y, m, 0);
         return Math.floor((d.getDate() - 1) / 7) + 1;
     }
 
     __getRomanNum(num) {
+        while (num > 12) {
+            num -= 12;
+        }
         let nums = [
             '0', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII'
         ];
         return nums[num];
     }
 
-    __getSameDays(date, dayNumber) {
+
+    __getSameDays(date1, dayNumber) {
+        let date = this.__cloneDate(date1);
         let month = date.getMonth(), sameDays = [];
         date.setDate(1);
         while (date.getDay() !== dayNumber) {
